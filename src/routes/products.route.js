846c91d020b58e89480efdce1c61e1cadd8bd313
Rafael <bechstedt@hotmail.com>
2023-03-01 18:25:43 -0300

    Req 04

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const validateProductName = require('../middlewares/validateProduct');
 
 const productsRouter = express.Router();
 
 productsRouter.get('/', productsController.getAllProducts);
 productsRouter.get('/:id', productsController.getProductById);
-productsRouter.post('/', productsController.insertProduct);
+productsRouter.post('/', validateProductName, productsController.insertProduct);
 
 module.exports = productsRouter;
